6b35ae065060dd20067b179c528cc954cf48235e
DavidM42 <davidmerz@gmx.de>
2020-08-04 15:12:03 +0200

    introduced favicons and manifest json icons

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ import favicon
 from base64 import b64encode
 
 
-def createManifest(subjectPath = None, subjectTitle = None, color = None, background_color = None):
+def createManifest(subjectPath = None, subjectTitle = None, color = None, background_color = None, toBasePath = '.'):
     data = {
         "name": "Wuel Quick Links",
         "short_name": "Wuel",
@@ -22,8 +22,45 @@ def createManifest(subjectPath = None, subjectTitle = None, color = None, backgr
         "display": "browser",
         "orientation": "portrait",
         "scope": "/",
-        "start_url": "/"
-        # TODO missing icons cause I don't have one yet
+        "start_url": "/",
+        "icons": [
+            {
+                "src": toBasePath + "/static/icons/android-icon-36x36.png",
+                "sizes": "36x36",
+                "type": "image/png",
+                "density": "0.75"
+            },
+            {
+                "src": toBasePath + "/static/icons/android-icon-48x48.png",
+                "sizes": "48x48",
+                "type": "image/png",
+                "density": "1.0"
+            },
+            {
+                "src": toBasePath + "/static/icons/android-icon-72x72.png",
+                "sizes": "72x72",
+                "type": "image/png",
+                "density": "1.5"
+            },
+            {
+                "src": toBasePath + "/static/icons/android-icon-96x96.png",
+                "sizes": "96x96",
+                "type": "image/png",
+                "density": "2.0"
+            },
+            {
+                "src": toBasePath + "/static/icons/android-icon-144x144.png",
+                "sizes": "144x144",
+                "type": "image/png",
+                "density": "3.0"
+            },
+            {
+                "src": toBasePath + "/static/icons/android-icon-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "density": "4.0"
+            }
+        ]
     }
     if subjectPath and subjectTitle and color and background_color:
         data["name"] = "Wuel - " + subjectTitle + " Links"
@@ -132,7 +169,7 @@ def renderSubjectPages():
                 outpath="dist/" + subject,
                 env_globals=data)
             site.render()
-            createManifest(subject,data["title"],data["color"], data["backgroundColor"])
+            createManifest(subject,data["title"],data["color"], data["backgroundColor"], '..')
     return subjectLinks
 
 def renderHomepage(subjectLinks):
