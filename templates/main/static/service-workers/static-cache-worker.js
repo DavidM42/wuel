cc41e55b516df052f140e118373e086c70394a52
DavidM42 <davidmerz@gmx.de>
2021-07-04 15:57:48 +0200

    feat: changed to new icon & pwa changes * updated links to new icons in manifest and index templates * also new service worker strategy with two seperate ones * one worker to cache static files like css and icons * one to cache index.html of subject and it's links.json, manifest.json

--- templates/main/static/service-workers/static-cache-worker.js
+++ templates/main/static/service-workers/static-cache-worker.js
@@ -6,26 +6,17 @@
 // still in this static folder to only be copied once
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-files-cache-v1';
 
-// inspired by https://stackoverflow.com/a/14462451
-const path = window.location.pathname
-const subjectPath = path.substr(0, path.lastIndexOf("/"));
-
-// path where static assets for all reside in
-const staticPath = subjectPath.substr(0, subjectPath.lastIndexOf("/")) + '/static';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
-    `${subjectPath}/index.html`,
-    `${subjectPath}/manifest.json`,
-    `${subjectPath}/courses.json`,
-    `${staticPath}/icons/favicon.ico`,
-    `${staticPath}/main.css`,
-    `${staticPath}/icons/favicon-96x96.png`,
-    `${staticPath}/icons/favicon-16x16.png`,
-    `${staticPath}/external/bootstrap.min.css`,
-    `${staticPath}/external/bootstrap-native.min.js`
+    `/icons/favicon.ico`,
+    `/main.css`,
+    `/icons/favicon-96x96.png`,
+    `/icons/favicon-16x16.png`,
+    `/external/bootstrap.min.css`,
+    `/external/bootstrap-native.min.js`
 ];
 
 self.addEventListener('install', (evt) => {
